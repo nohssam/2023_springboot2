9c1ba14b4149dc2ff9014990f60824e502ea8bc7
nohssam73 <nohssam73@gmail.com>
2023-09-02 18:08:11 +0900

    PasswordEncoder deprecated 수정

--- src/main/java/com/ict/edu2/config/SecurityJavaConfig.java
+++ src/main/java/com/ict/edu2/config/SecurityJavaConfig.java
@@ -9,17 +9,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+
+// @Configuration이라고 하면 설정파일을 만들기 위한 애노테이션 or Bean을 등록하기 위한 애노테이션이다.
+// @EnableWebSecurity 스프링 시큐리티 사용을 위한 어노테이션 선언 @Configuration 생략 가능
+// @Configuration
 @EnableWebSecurity
-public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
-    }   
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class SecurityJavaConfig  {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
         http
         .csrf().disable()
         .authorizeRequests()
@@ -31,8 +30,10 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
             .formLogin()
             .disable()
             .cors();
+       
+            return http.build();
     }
-
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
